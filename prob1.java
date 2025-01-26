cc5e553e21a8a53a1331bba46f5380fb0d893382
shivangiS04 <shivangimasterblaster@gmail.com>
2025-01-27 01:24:53 +0530

    git commit

---
+++ prob1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if(root == null) return ans;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            int max = Integer.MIN_VALUE;
+            for(int i =0;i<size;i++){
+                TreeNode curr = q.remove();
+                if(curr.left!=null) q.add(curr.left);
+                if(curr.right!=null)q.add(curr.right);
+                max =Math.max(max,curr.val);
+            }
+            ans.add(max);
+        }
+        return ans;
+    }
+}
